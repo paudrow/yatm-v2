82fe0658f1ea5e7d602b9aa513b3ed5324cc593f
Tully Foote <tullyfoote@intrinsic.ai>
2025-05-20 15:08:28 -0700

    successfully reentering and updating issues

---
+++ src/yatm_v2/src/helpers/get_local_issues_matches.rs
@@ -0,0 +1,83 @@
+use crate::types::LocalIssue;
+use octocrab::{models::issues::Issue as GithubIssue, service::body};
+use predicates::path::missing;
+
+/// The types of matches between a local issue and a github issue
+/// Canonical Match is matching permutation and shortname
+#[derive(Eq, PartialEq)]
+pub enum IssueMatchType {
+    Missing, // No equivalant GitHub Issue found
+    Match, // Matching github issue found
+    MatchedWithDiff, // Matching GitHub Issue found but with some differences
+}
+
+pub struct GithubIssueMatches {
+    pub local_issue: LocalIssue,
+    pub github_issue: Option<GithubIssue>,
+    pub match_type: IssueMatchType,
+}
+
+/// Get local issues that match upstream
+pub fn get_local_issues_matches(
+    local_issues: &Vec<LocalIssue>,
+    github_issues: &Vec<GithubIssue>,
+) -> Vec<GithubIssueMatches> {
+    let mut results: Vec<GithubIssueMatches> = Vec::<GithubIssueMatches>::new();
+    for local_issue in local_issues {
+        let gh_issue = github_issues.iter().find(|i| is_local_issue_match_github_issue(local_issue, i));
+
+        if gh_issue.is_some() {
+            results.push(GithubIssueMatches{
+                local_issue: local_issue.clone(),
+                github_issue: gh_issue.clone().cloned(),
+                match_type: if is_local_issue_identical_github_issue(local_issue, gh_issue.unwrap()) { IssueMatchType::Match} else { IssueMatchType::MatchedWithDiff},
+            });
+        }
+        else {
+            results.push(GithubIssueMatches{
+                local_issue: local_issue.clone(),
+                github_issue: None,
+                match_type: IssueMatchType::Missing,
+            });
+        }
+    }
+    results
+}
+
+fn is_local_issue_match_github_issue(
+    local_issue: &LocalIssue,
+    github_issue: &GithubIssue,
+) -> bool {
+    for label in local_issue.labels.iter() {
+        let github_issue_label_names = github_issue
+        .labels
+        .iter();
+        if !github_issue
+            .labels
+            .iter()
+            .any(|github_label| &github_label.name == label)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn is_local_issue_identical_github_issue(
+    local_issue: &LocalIssue,
+    github_issue: &GithubIssue,
+) -> bool {
+
+    let title_match: bool = 
+        if local_issue.title == github_issue.title { true } else { false };
+        println!("title_compare [{}] [{}]", local_issue.title, github_issue.title);
+
+    let body_match: bool =
+        if local_issue.text_body == github_issue.body.clone().unwrap_or("".to_string() ) { true } else { false };
+        println!("body_compare AAAAAA\n {} \n BBBBBBB \n {} \n CCCCCCC", local_issue.text_body, github_issue.body.as_ref().unwrap_or(&"unwrap failed".to_string()));
+        println!("{:?}", github_issue.body);
+
+    // Full match
+    title_match && body_match
+
+}
--- src/yatm_v2/src/helpers/mod.rs
+++ src/yatm_v2/src/helpers/mod.rs
@@ -1,4 +1,5 @@
 mod get_files;
+mod get_local_issues_matches;
 mod get_local_issues_without_matches;
 mod get_test_cases;
 mod make_test_cases;
@@ -7,6 +8,9 @@ mod test_case_to_markdown;
 mod test_cases_builder;
 
 pub use get_files::get_files;
+pub use get_local_issues_matches::get_local_issues_matches;
+pub use get_local_issues_matches::GithubIssueMatches;
+pub use get_local_issues_matches::IssueMatchType;
 pub use get_local_issues_without_matches::get_local_issues_without_matches;
 pub use get_test_cases::get_test_cases;
 pub use make_test_cases::make_test_cases;
